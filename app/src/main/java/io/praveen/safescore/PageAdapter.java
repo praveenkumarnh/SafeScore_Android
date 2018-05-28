c4af5d30026489abd598c1a363e6d761e92f2805
Praveen Thirumurugan <thirupraveen18@gmail.com>
2018-05-28 11:52:22 +0530

    Initial commit

---
+++ app/src/main/java/io/praveen/safescore/PageAdapter.java
@@ -0,0 +1,33 @@
+package io.praveen.safescore;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+public class PageAdapter extends FragmentStatePagerAdapter {
+
+    private int mNumOfTabs;
+
+    public PageAdapter(FragmentManager fm, int NumOfTabs) {
+        super(fm);
+        this.mNumOfTabs = NumOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position) {
+            case 0:
+                return new MainActivity();
+            case 1:
+                return new HelpActivity();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return mNumOfTabs;
+    }
+}
